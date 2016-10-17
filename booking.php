a2137166301a4444ec706868f60e57a520aeb440
Afzal Ashraf <mafzalashraf4044@gmail.com>
2016-10-17 11:41:28 +0500

    form validation added in booking.php

--- booking.php
+++ booking.php
@@ -37,39 +37,118 @@
 							<?php } ?>
 						</div>
 
+						<!-- Database Logic For Online Booking Form Submission -->
+
+						<?php
+							$inputs_array = array();
+							$inputs_value = array();
+							$show_dialog_box2 = false;
+						  $errors = false;
+
+						  //This code will run when the user submits the form and the page reloads(b/c of action att set to PHP_SELF)
+						  if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['submit'] == 'Booking'){
+						    $show_dialog_box = true;
+
+								foreach ($_POST as $key => $value) {
+									$inputs_array[] = validate_input($value);
+
+								}
+
+								foreach ($inputs_array as $input_array) {
+
+									if($input_array['err_msg'] != NULL){
+										$errors = true;
+									}
+
+									$inputs_value[] = $input_array['input_value'];
+
+								}
+
+
+								for ($i=0; $i < count($inputs_array); $i++) {
+									echo $inputs_array[$i]['input_value'];
+								}
+
+					      $sql = "INSERT INTO online_table_booking (no_of_person, time, day, name, ph_no, email)
+					      VALUES ('$inputs_array[0]=>input_value', '$inputs_value[1]', '$inputs_value[2]', '$inputs_value[3]', '$inputs_value[4]', '$inputs_value[5]')";
+
+						      if (!$errors) {
+						        $conn->query($sql);
+										echo "added successfully!";
+						      }else {
+									    echo "Error: " . $sql . "<br>" . $conn->error;
+									}
+
+						    $conn->close();
+						  }
+						 ?>
+
+						 <!-- Dialog box for Form Submission msg -->
+						 <div id="modal2" class="modal">
+
+						   <div class="modal-content">
+
+						   <?php if($errors) {?>
+						     <h4>Booking Form Submission Failed !</h4>
+						     <div class="divider"></div>
+								 <?php for ($i=0; $i < count($inputs_array); $i++) { ?>
+										 <p><?php if($inputs_array['err_msg'] != NULL){echo $inputs_array['err_msg'];} ?></p>
+								 <?php } ?>
+
+						   <?php } ?>
+
+						   <?php if(!$errors) {?>
+						     <h4>Dear Customer !</h4>
+						     <div class="divider"></div>
+						     <p>Your Booking Request has been received, You will be informed after confirmation! Thank You</p>
+						   <?php } ?>
+
+						   </div>
+						   <div class="modal-footer">
+						     <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+						   </div>
+						 </div>
+
+
 						<div class="row form-area">
-							<form class="col s12">
+							<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="col s12">
+
 								<div class="row">
 									<div class="input-field col s4">
-										<input placeholder="" id="no-of-person" value="" type="text" class="validate">
+										<input placeholder="" id="no-of-person" name="no_of_person" required value="" type="text" class="validate">
 										<label for="no-of-person">Number of Person</label>
+										<i class="hidden fa fa-warning"> <span>Number of Person is Required!</span></i>
 									</div>
 									<div class="input-field col s4">
-										<input placeholder="" id="time" type="text" class="validate">
+										<input placeholder="" id="time" name="time" required type="time" class="validate">
 										<label for="time">Time</label>
+										<i class="hidden fa fa-warning"> <span>Time is Required!</span></i>
 									</div>
 									<div class="input-field col s4">
-										<input placeholder="" id="day" type="text" class="validate">
+										<input placeholder="" id="day" name="day" required type="text" class="validate">
 										<label for="day">Day</label>
+										<i class="hidden fa fa-warning"> <span>Day is Required!</span></i>
 									</div>
 								</div>
 
 								<div class="row">
 									<div class="input-field col s4">
-										<input placeholder="" id="name" type="text" class="validate">
+										<input placeholder="" id="name" name="name" required type="text" class="validate">
 										<label for="name">Enter your name</label>
+										<i class="hidden fa fa-warning"> <span>Name is Required!</span></i>
 									</div>
 									<div class="input-field col s4">
-											<input placeholder="" id="phone-number" type="text" class="validate">
+										<input placeholder="" id="phone-number" required name="ph_no" type="text" class="validate">
 										<label for="phone-number">Phone number</label>
+										<i class="hidden fa fa-warning"> <span>Phone Number is Required!</span></i>
 									</div>
 									<div class="input-field col s4">
-										<input placeholder="" id="email" type="text" class="validate">
+										<input placeholder="" id="email" name="email" required type="text" class="validate">
 										<label for="email">Email id</label>
+										<i class="hidden fa fa-warning"> <span>Email is Required!</span></i>
 									</div>
 								</div>
-
-								<button class="waves-effect waves-light btn right white-text">Book Now</button>
+								<input type="submit" class="waves-effect waves-light btn" name="submit" value="Booking">
 							</form>
 						</div>
 					</div>
@@ -97,6 +176,17 @@
 
 	</div>
 </div>
+
+<!-- Opening model On Booking Form Submission -->
+    <?php if($show_dialog_box2) {?>
+      <script type="text/javascript">
+        $(document).ready(function(){
+					$('#modal2').openModal();
+          // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+        });
+      </script>
+    <?php } ?>
+
 <?php
 include('./includes/footer.php');
 ?>
--- scripts/booking.js
+++ scripts/booking.js
@@ -17,3 +17,20 @@ $(
         });
     }
 );
+
+
+
+//Form Validation for Subscription Form In footer
+
+$('input').on("invalid", function(e) {
+    e.preventDefault();
+    console.log(e.target);
+    $(e.target).next().next().removeClass('hidden');
+    $(e.target).next().next().addClass('error');
+});
+
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
